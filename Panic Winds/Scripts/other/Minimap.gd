f273b2f1742f2a74dd99385d498bb88bc758e262
Samuel Holden <88010672+USERDOT1@users.noreply.github.com>
2024-08-25 18:10:10 -0700

    Changed Minimap Display

--- Panic Winds/Scripts/other/Minimap.gd
+++ Panic Winds/Scripts/other/Minimap.gd
@@ -1,4 +1,4 @@
 extends ColorRect
 
 func _process(delta):
-	$Player.position = GlobalVars.currentPlayer.global_position/120 + Vector2(550,550)
+	$Player.position = GlobalVars.currentPlayer.global_position/137 + Vector2(562.5,552.5)
